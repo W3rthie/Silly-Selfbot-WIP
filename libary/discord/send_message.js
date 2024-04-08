2a19b8252e5ef5f18e8fc4a471b09c0ccd8fa994
Werthie <99995419+W3rthie@users.noreply.github.com>
2024-04-09 00:11:26 +0100

    Add files via upload

---
+++ libary/discord/send_message.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config()
+
+const discord_token = process.env.discord_token
+
+exports.main = function(data) {
+    const {channel_id, content} = data
+    const url = `https://discord.com/api/v10/channels/${channel_id}/messages`
+
+    fetch(url, {
+        method: "POST",
+
+        headers: {
+            "Authorization": discord_token,
+            "Content-Type": "application/json"
+        },
+    
+        body: JSON.stringify({"content": content})
+    })
+}
