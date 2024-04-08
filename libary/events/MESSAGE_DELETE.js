d35af87d43167fb1226bee2310e62d4a3bb3de19
Werthie <99995419+W3rthie@users.noreply.github.com>
2024-04-09 00:53:49 +0100

    Update MESSAGE_DELETE.js

--- libary/events/MESSAGE_DELETE.js
+++ libary/events/MESSAGE_DELETE.js
@@ -1,10 +1,32 @@
+const log_channel = require("../../config.json").log_channel
 const saved_messages = require("./MESSAGE_CREATE").saved_messages
+const send_message = require("../discord/send_message").main
 
 exports.main = function(data) {
     const {id} = data
     const message_data = saved_messages[id]
 
     if (message_data) {
-        console.log("Deleted message: ", message_data)
+        const {author, content, channel_id} = message_data
+
+        send_message({
+            "channel_id": log_channel,
+            "content": 
+`
+${"```diff"}
+! [System] - Deleted Message
+
+Display Name: ${author.global_name}
+Username: ${author.username}
+User ID: ${author.id}
+
+Message ID: ${id}
+Channel ID: ${channel_id}
+
+// Message Content:
+${content}
+${"```"}
+`
+        })
     }
 }
